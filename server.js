28eb7e73b6212ab2918a39d39a659f9cfb1f8c57
bmmishra <136698041+bmmishra@users.noreply.github.com>
2024-05-26 21:42:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(_ _dirname,'./client/build')))
 
 
 // Routes
@@ -30,7 +30,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/product", productRoutes)
 
 app.use("*", function (req, res) {
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+    res.sendFile(path.join(_ _dirname,"./client/build/index.html"))
 })
 
 
