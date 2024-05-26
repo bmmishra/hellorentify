262b85cb2a11fc532ee2733104c9b27fc5fcfb66
bmmishra <136698041+bmmishra@users.noreply.github.com>
2024-05-26 21:52:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 //import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect("mongodb+srv://himanshu1441996:H123456789h@cluster1.anmnixu.mongodb.net/latest_ecom?retryWrites=true&w=majority");
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`
     );
